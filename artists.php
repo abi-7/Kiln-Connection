d5c28e23cbb30f9d874ad76a24679ba3a94afe24
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-08 21:10:45 -0500

    Create artists.php

---
+++ artists.php
@@ -0,0 +1,32 @@
+<?php
+
+/*******w******** 
+    
+    Name: Abigail Ferreira
+    Date: 2024-02-19
+    Description: main page to explore artists
+
+****************/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+<?php include('nav.php'); ?>
+
+<main>
+
+</main>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
