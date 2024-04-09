a0f74ccc51813ce642c5e69c94df738eaf8a9be8
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-08 21:07:06 -0500

    Create items.php

---
+++ items.php
@@ -0,0 +1,32 @@
+<?php
+
+/*******w******** 
+    
+    Name: Abigail Ferreira
+    Date: 2024-04-06
+    Description: marketplace tab/page for the site, where users can purchase pottery items
+
+****************/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+<?php include('nav.php'); ?>
+
+<main>
+
+</main>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
