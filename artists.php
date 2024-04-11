bfe24e800e32bfee27d1d247a4aba25e0c6d3911
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-11 00:27:39 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -8,6 +8,19 @@
 
 ****************/
 
+require('connect.php');
+
+// SQL is written as a String.
+//chronological order 5 most recent posts
+$query = "SELECT * FROM artist ORDER BY first_name ASC LIMIT 10";
+
+// Prepare and execute the default query
+$statement = $db->prepare($query);
+
+// Execute the query
+$statement->execute(); 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +36,35 @@
 <?php include('nav.php'); ?>
 
 <main>
+    <h1>Local Artists</h1>
+        <form class="search-container" action="" method="GET">
+            <input
+             type="text"
+             class="search-bar"
+             placeholder="Search by artist name ..."
+            />
+            <button type="submit" class="search-button">Search</button>
+        </form>
 
+                        <!--if no entries yet-->
+    <?php if($statement->rowCount() == 0) : ?>
+        <div>
+            <p>No artists yet!</p>
+        </div>
+    <?php else:?>
+        <ul>
+        <?php while($row = $statement->fetch()): ?>
+            <li>
+                <h3>
+                <?= $row['first_name'] . ' ' . $row['last_name'] ?>
+                </h3>
+                <small>
+                <?= $row['home_studio'] ?>
+                </small>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+        <?php endif; ?>
 </main>
 
 <?php include('footer.php'); ?>
