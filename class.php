32ec1172ab7a0e563f31d038f60f49085f6089cc
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-11 00:26:35 -0500

    Update class.php

--- class.php
+++ class.php
@@ -9,6 +9,17 @@
 
 ****************/
 
+require('connect.php');
+
+// SQL is written as a String.
+//chronological order 5 most recent posts
+$query = "SELECT * FROM class ORDER BY class_name ASC LIMIT 10";
+
+// A PDO::Statement is prepared from the query.
+$statement = $db->prepare($query);
+
+// Execution on the DB server is delayed until we execute().
+$statement->execute(); 
 
 ?>
 
@@ -25,7 +36,6 @@
 <?php include('nav.php'); ?>
 
 <main>
-<div>
     <h1>Available Classes</h1>
         <div class="search-container">
             <input
@@ -35,6 +45,28 @@
             />
             <button type="submit" class="search-button">Search</button>
         </div>
+
+                                        <!--if no entries yet-->
+    <?php if($statement->rowCount() == 0) : ?>
+        <div>
+            <p>No classes have been posted yet,
+                check back later!
+            </p>
+        </div>
+    <?php else:?>
+        <ul>
+        <?php while($row = $statement->fetch()): ?>
+            <li>
+                <h3>
+                <?= $row['item_name'] ?>
+                </h3>
+                <small>
+                <?= $row['item_cost'] ?>
+                </small>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+        <?php endif; ?>
 </main>
 
 <?php include('footer.php'); ?>
