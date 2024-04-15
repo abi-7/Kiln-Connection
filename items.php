9e090f87cb47c62adf5ccaf16a1631e1fe65f148
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-15 10:32:28 -0500

    Query update items.php
    update how data is shown

--- items.php
+++ items.php
@@ -12,7 +12,13 @@ require('connect.php');
 
 // SQL is written as a String.
 //chronological order 5 most recent posts
-$query = "SELECT * FROM item ORDER BY item_name ASC LIMIT 10";
+$query = "
+    SELECT Item.*, PotteryType.type_name AS pottery_type
+    FROM Item
+    INNER JOIN PotteryType ON Item.type_id = PotteryType.type_id
+    ORDER BY Item.item_name ASC
+    LIMIT 20
+";
 
 // A PDO::Statement is prepared from the query.
 $statement = $db->prepare($query);
@@ -48,16 +54,19 @@ $statement->execute();
                                 <!--if no entries yet-->
     <?php if($statement->rowCount() == 0) : ?>
         <div>
-            <p>No artists yet!</p>
+            <p>No items yet!</p>
         </div>
     <?php else:?>
-        <ul>
+        <ul class="grid-container">
         <?php while($row = $statement->fetch()): ?>
-            <li>
+            <li class="grid-item">
                 <h3>
                 <?= $row['item_name'] ?>
                 </h3>
-                <small>
+                <small>Technique:
+                <?= $row['pottery_type'] ?>
+                </small>
+                <small>$
                 <?= $row['item_cost'] ?>
                 </small>
             </li>
