8b2d3c7def7ead779b261235acfb82c29ef28741
Abigail Ferreira <95972173+abi-7@users.noreply.github.com>
2024-04-23 12:13:05 -0500

    Add search function

--- class.php
+++ class.php
@@ -21,6 +21,34 @@ $statement = $db->prepare($query);
 // Execution on the DB server is delayed until we execute().
 $statement->execute(); 
 
+$searchErr = '';
+$class_details='';
+if(isset($_POST['save']))
+{
+	if(!empty($_POST['search']))
+	{
+        $search = htmlspecialchars($_POST['search']);
+		$searchQuery = "
+            SELECT * FROM class WHERE class_name LIKE :search OR artist_teacher LIKE :search
+        ";
+        $stmt = $db->prepare($searchQuery);
+
+        $stmt->execute(array(':search' => "%$search%"));
+        // Fetch results
+        $class_details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+	else
+	{
+		$searchErr = "Please enter the information";
+	}
+   
+}
+
+// Check if the clear button is clicked
+if (isset($_POST['clear'])) {
+    $class_details = '';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -36,17 +64,28 @@ $statement->execute();
 <?php include('nav.php'); ?>
 
 <main>
+<form action="#" method="post">
     <h1>Available Classes</h1>
         <div class="search-container">
             <input
              type="text"
              class="search-bar"
-             placeholder="Search by ..."
+             name="search"
+             placeholder="Search by class or teacher name..."
             />
-            <button type="submit" class="search-button">Search</button>
+            <button type="submit" name="save" class="search-button">Search</button>
+            <button type="submit" name="clear" class="search-button">Reset</button>
         </div>
+</form>
 
-                                        <!--if no entries yet-->
+    <!--search error-->
+    <?php if(!empty($searchErr)) : ?>
+        <div>
+             <p><?php echo $searchErr; ?></p>
+        </div>
+    <?php endif; ?>
+
+    <!--if no entries yet-->
     <?php if($statement->rowCount() == 0) : ?>
         <div>
             <p>No classes have been posted yet,
@@ -54,6 +93,29 @@ $statement->execute();
             </p>
         </div>
     <?php else:?>
+    <!--display based off search-->
+    <?php if(!empty($class_details)) : ?>
+    <h2>Search Results</h2>
+    <ul class="grid-container">
+        <?php foreach($class_details as $class) : ?>
+            <li class="grid-item">
+                <h3>
+                <?= $class['class_name'] ?>
+                </h3>
+                <small>
+                <?= $class['class_level'] ?>
+                </small>
+                <small>Teacher: 
+                <?= $class['artist_teacher'] ?>
+                </small>
+                <small>$
+                <?= $class['class_cost'] ?>
+                </small>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <?php else: ?>
+        <!--default display-->
         <ul class="grid-container">
         <?php while($row = $statement->fetch()): ?>
             <li class="grid-item">
@@ -72,6 +134,7 @@ $statement->execute();
             </li>
             <?php endwhile; ?>
         </ul>
+        <?php endif; ?>
         <?php endif; ?>
 </main>
 
